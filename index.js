57fab7bdafe1719858907d14bb5ab75ace0f1c48
Elf Sundae <elf.sundae@gmail.com>
2016-09-28 23:32:18 +0800

    Default output to src

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ Elixir.config.replace = {
 };
 
 Elixir.extend('replace', function(src, search, replacement, output, options) {
-  var paths = new Elixir.GulpPaths().src(src).output(output);
+  var paths = new Elixir.GulpPaths().src(src).output(output || src);
 
   new Elixir.Task('replace', function($) {
       this.recordStep('Replacing "' + search + '" to "' + replacement + '"');
